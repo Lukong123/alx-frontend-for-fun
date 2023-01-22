2bdfe67194813e814e98aba7314d7700b9f72e72
anne <annemachinda@gmail.com>
2023-01-22 11:25:56 +0100

    task 0

---
+++ markdown2html.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+import sys
+
+n = len(sys.argv)
+
+if n < 2:
+    sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
+    exit(1)
+if(FileExistsError):
+    sys.stderr.write("Missing <filename>")
+    exit(1)
+else:
+    exit(0)
