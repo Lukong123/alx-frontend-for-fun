46232e90da3a69c8feb87695f19da8d7ca429cb6
anne <annemachinda@gmail.com>
2023-01-22 11:54:49 +0100

    task 0

--- markdown2html.py
+++ markdown2html.py
@@ -6,14 +6,17 @@ Second argument is the output file name
 
 
 import sys
+import os.path
 
-n = len(sys.argv)
+if __name__ == '__main__':
 
-if n < 3:
-    sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
-    exit(1)
-if(FileExistsError):
-    sys.stderr.write("Missing <filename>")
-    exit(1)
-else:
-    exit(0)
+    n = len(sys.argv)
+
+    if n < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        exit(1)
+    if not os.path.isfile(sys.argv[1]):
+        print("Missing {}".format(sys.argv[1]), file=sys.stderr)
+        exit(1)
+    else:
+        exit(0)
